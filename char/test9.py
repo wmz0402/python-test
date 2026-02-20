9cee9d6753a7e2553171a83472d9256a4c00f3d8
wmz0402 <491453898@qq.com>
2026-02-20 17:11:42 +0800

    str重写

--- char/test9.py
+++ char/test9.py
@@ -188,30 +188,56 @@
 # son.showA()
 # son.showB()
 #
-#多态=====================================================================
-class Person():
-    def eat(self):
-        print("人吃五谷杂粮")
+# #多态=====================================================================
+# class Person():
+#     def eat(self):
+#         print("人吃五谷杂粮")
+#
+# class Cat():
+#     def eat(self):
+#         print("猫喜欢吃鱼")
+# class Dog():
+#     def eat(self):
+#         print("狗喜欢吃骨头")
+# #这三个类中都有一个同名的方法，eat
+# #编写函数
+# def fun(obj): #obj是函数的形式参数，在定义处不知道数据类型
+#     obj.eat()
+# #创建三个类对象
+# per=Person()
+# cat=Cat()
+# dog=Dog()
+# #调用fun函数
+# fun(per)
+# fun(cat)
+# fun(dog)
 
-class Cat():
-    def eat(self):
-        print("猫喜欢吃鱼")
-class Dog():
-    def eat(self):
-        print("狗喜欢吃骨头")
-#这三个类中都有一个同名的方法，eat
-#编写函数
-def fun(obj): #obj是函数的形式参数，在定义处不知道数据类型
-    obj.eat()
-#创建三个类对象
-per=Person()
-cat=Cat()
-dog=Dog()
-#调用fun函数
-fun(per)
-fun(cat)
-fun(dog)
+# #查看指定对象的属性
+# class Person(object):
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#     def show(self):
+#         print(f"大家好，我叫{self.name},我今年{self.age}岁了")
+# #创建Person类对象
+# per=Person("陈美美",20)
+# print(dir(per))
 
+
+#__str__方法的重写
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    #方法重写
+    def __str__(self):
+        return "这是个人类，具有name和age两个实例属性"  #返回值是一个字符串
+
+#创建类对象
+per=Person("陈美美",20)
+print(per.__str__())
 
 
 
