cdd81781aa68e01ace6f4ca6b7b293c9751cba8d
wmz0402 <491453898@qq.com>
2026-02-24 17:48:18 +0800

    random模块的使用

--- char/test10.py
+++ char/test10.py
@@ -51,10 +51,26 @@
 
 
 
-
-
-
-
+# #random模块的使用
+# import random
+# random.seed(10)
+# print(random.random()) #[0.0,1.0)
+# print(random.random())
+# print("-"*30)
+# random.seed(10)
+# print(random.randint(1,100))
+#
+#
+# for i in range(10):
+#     print(random.randrange(1,10,3))
+#
+# print(random.uniform(1,100))#[a,b]随即小数
+#
+# lst=[i for i in range(1,11)]
+# print(random.choice(lst)) #lst是列表，称为序列
+# #随机的排序
+# random.shuffle(lst)
+# print(lst)
 
 
 
