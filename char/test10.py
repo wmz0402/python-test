515af6ffcef81c993f78975229fb91a40004453f
wmz0402 <491453898@qq.com>
2026-02-25 19:00:49 +0800

    第三方模块即练习

--- char/test10.py
+++ char/test10.py
@@ -168,17 +168,146 @@
 # for item in lst:
 #     print(item)
 
-#爬取百度logo
-import requests
-url="https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
-resp=requests.get(url)
-#保存到本地
-with open("logo.png","wb")as file:
-    file.write(resp.content)
+# #爬取百度logo
+# import requests
+# url="https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
+# resp=requests.get(url)
+# #保存到本地
+# with open("logo.png","wb")as file:
+#     file.write(resp.content)
+
+
+# #从pdf文件中提取数据
+# import pdfplumber
+# #打开pdf文件
+# with pdfplumber.open("高数A1自测题4.pdf") as pdf:
+#     for i in pdf.pages:
+#         print(i.extract_text()) #提取内容
+#         print(f"----------------------------第{i.page_number}页结束")
+
+# #对图像进行灰度处理
+# #Numpy模块
+# import  numpy as np
+# import matplotlib.pyplot as plt
+# #读取图片
+# n1=plt.imread("iphone1.jpg")
+# print(type(n1),n1) #数组，三维数组，最高维度表示的是图像的高，次高纬度表示的是图像的宽，最低维度颜色
+# plt.imshow(n1)
+#
+# #编写一个灰度的公式
+# n2=np.array([0.29,0.587,0.114]) #创建数组
+# #将数组n1颜色值与数组n2进行点乘运算
+# x=np.dot(n1,n2)
+# #传入数组，显示灰度
+# plt.imshow(x,cmap="gray")
+# #显示图像
+# plt.show()
+
+
+
+# #Pandas和Matplotlib模块
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+
+# #饼状图
+# from pyecharts import options as opts
+# from pyecharts.charts import Pie
+# from pyecharts.faker import Faker
+#
+# # --- 新增下面这两行代码 ---
+# from pyecharts.globals import CurrentConfig
+# CurrentConfig.ONLINE_HOST = "https://cdn.staticfile.net/echarts/5.4.2/"
+# # ---------------------------
+#
+# # 准备数据 (下面的代码基本保持不变)
+# lst = [["华为", 1000], ["OPPO", 1200], ["苹果", 300], ["小米", 980]]
+# c = (
+#     Pie()
+#     # 注意这里顺手帮你加了个参数名 data_pair=，这样写更标准，不容易报错
+#     .add(series_name="", data_pair=lst)
+#     .set_global_opts(title_opts=opts.TitleOpts(title="2028年北京手机出库占比情况"))
+#     .set_series_opts(label_opts=opts.LabelOpts(formatter="{b}: {c}%"))
+#     .render("phone.html")
+# )
 
 
 
 
+# #PIL  颜色交换
+# from PIL import Image
+# #加载图片
+# im=Image.open('iphone1.jpg')
+# #提取RGB图像的颜色通道，返回结果试图向副本
+# r,g,b=im.split()
+# #合并通道
+# om=Image.merge(mode='RGB',bands=(r,b,g))
+# om.save('test.png')
+#
+
+
+#jieba 对中文进行分词的模块
+
+# #实战一  订票系统
+# import prettytable as pt
+# from numpy.ma.core import choose
+#
+#
+# #显示坐席
+# def show_ticket(row_num):
+#     tb=pt.PrettyTable() #创建一张表格
+#     #设置标题（表格的排头部分）
+#     tb.field_names=["行号","座位1","座位2","座位3","座位4","座位5"]
+#     #遍历有票
+#     for i in range(1,row_num+1):
+#         lst=[f"第{i}行","有票","有票","有票","有票","有票"]
+#         tb.add_row(lst)
+#     print(tb)
+#
+# #订票
+# def order_ticket(row_num,row,column):
+#     tb = pt.PrettyTable()  #创建一张表单
+#     tb.field_names = ["行号","座位1","座位2","座位3","座位4","座位5"]
+#     for i in range(1,row_num+1):
+#         if int(row)==i:
+#             lst=[f"第{i}行","有票","有票","有票","有票","有票"]
+#             lst[int(column)]="已售"
+#             tb.add_row(lst)
+#         else:
+#             lst = [f"第{i}行", "有票", "有票", "有票", "有票", "有票"]
+#             tb.add_row(lst)
+#     print(tb)
+#
+#
+# if __name__ == '__main__':
+#     row_num=6
+#     show_ticket(row_num)
+# #开始售票
+#     choose_num=input("请输入您选择的坐席：如4,3表示四排三列")
+#     row,column=choose_num.split(",")
+#     order_ticket(row_num,row,column)
+#
+
+
+
+#实战二 推算几天后的日期
+import datetime
+#输入日期
+def input_date():
+    inputdate=input("请输入开始日期：（20201001）输完后请按回车")
+    datestr=inputdate[0:4]+"-"+inputdate[4:6]+"-"+inputdate[6:8]
+    #类型转换
+    dt=datetime.datetime.strptime(datestr,"%Y-%m-%d")
+    return dt
+
+
+if __name__=="__main__":
+    # print(input_date())
+    date=input_date()
+    # 输入间隔天数
+    in_num = eval(input("请输入间隔天数："))
+    date=date+datetime.timedelta(days=in_num)
+    print("您推算的日期是：",date)
 
 
 
